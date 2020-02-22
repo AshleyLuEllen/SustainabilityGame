cfb535e913949c18767d4fb340722532ea58c86a
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-02-22 14:49:05 -0600

    End screen foundation and score system

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let pages;
 let mapLocations;
 let fadeTime = 1000;
-let currentRoom = "homePage";
+let currentRoom;
+let score;
+let lastClick;
 
 $.getJSON("json/data.json", data => {
     pages = data.pages;
@@ -9,7 +11,14 @@ $.getJSON("json/data.json", data => {
 });
 
 $(document).ready(() => {
-    $("#content").fadeIn(fadeTime / 2);
+    $(".start-menu-text").hide();
+    $(".start-menu-buttons").hide();
+
+    $("#content").fadeIn(fadeTime / 2, () => {
+        $(".start-menu-text").fadeIn(fadeTime / 2, () => {
+            $(".start-menu-buttons").fadeIn(fadeTime / 2);
+        });
+    });
 
     $(".page").click(event => {
         let mouseX = event.pageX - $(event.target).offset().left;
@@ -18,12 +27,22 @@ $(document).ready(() => {
         if (pages[$(event.target).attr("id")]) {
             for (let el of pages[$(event.target).attr("id")].clickLocations) {
                 if (el.x1 <= mouseX && mouseX <= el.x2 && el.y1 <= mouseY && mouseY <= el.y2) {
+                    lastClick = el;
+
                     $.alert({
                         title: el.title,
-                        content: el.description,
+                        content: `<p>${el.description}</p>` + (!el.score ? `<p><em>Congratulations! You earned ${el.points} points!</em></p>` : ""),
                         useBootstrap: false,
-                        icon: `fa fa-${el.icon}`
+                        icon: `fa fa-${el.icon}`,
+                        animation: "scale",
+                        closeAnimation: "zoom",
+                        animateFromElement: false,
+                        type: el.score ? "blue" : "green",
+                        typeAnimated: true,
+                        theme: "material",
+                        onClose: addScore
                     });
+
                     break;
                 }
             }
@@ -43,6 +62,12 @@ $(document).ready(() => {
             } 
         }
     });
+
+    $(".end-game").click(e => {
+        endGame();
+    });
+
+    $(".score-flyer").hide();
 });
 
 function showInstructions() {
@@ -50,7 +75,10 @@ function showInstructions() {
 }
 
 function startGame() {
+    currentRoom = "homePage";
+    score = 0;
     changeToRoom("livingRoom");
+    updateScore();
 }
 
 function changeToRoom(room) {
@@ -59,4 +87,31 @@ function changeToRoom(room) {
     $(`#${currentRoom}`).fadeOut(fadeTime);
     $(`#${room}`).fadeIn(fadeTime);
     currentRoom = room;
+}
+
+function updateScore() {
+    $(".score").text(score);
+}
+
+function addScore() {
+    if (!lastClick.score) {
+        lastClick.score = true;
+        score += lastClick.points;
+
+        $(".score-flyer").text(`+${lastClick.points}`);
+
+        $(".score-flyer").show();
+    
+        $(".score-flyer").addClass("fly");
+    
+        setInterval(() => {
+            $(".score-flyer").hide();
+            $(".score-flyer").removeClass("fly");
+            updateScore();
+        },800);
+    }
+}
+
+function endGame() {
+    changeToRoom("endGame");
 }
