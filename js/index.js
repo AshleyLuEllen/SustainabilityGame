6b5a2fb85759d90eb3001965f2a394e8b5e82f19
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-02-22 13:31:09 -0600

    Finished pagination framework

--- js/index.js
+++ js/index.js
@@ -1,5 +1,43 @@
+let pages;
+let mapLocations;
+let fadeTime = 1000;
+let currentRoom = "homePage";
+
+$.getJSON("json/data.json", data => {
+    pages = data.pages;
+    mapLocations = data.mapLocations;
+});
+
 $(document).ready(() => {
-    $("#content").fadeIn(500);
+    $("#content").fadeIn(fadeTime / 2);
+
+    $(".page").click(event => {
+        let mouseX = event.pageX - $(event.target).offset().left;
+        let mouseY = event.pageY - $(event.target).offset().top;
+
+        if (pages[$(event.target).attr("id")]) {
+            for (let el of pages[$(event.target).attr("id")].clickLocations) {
+                if (el.x1 <= mouseX && mouseX <= el.x2 && el.y1 <= mouseY && mouseY <= el.y2) {
+                    alert(el.description);
+                    break;
+                }
+            }
+        }
+
+        console.log(`Click! ${mouseX} ${mouseY}`);
+    });
+
+    $(".map").click(event => {
+        let mouseX = event.pageX - $(event.target).offset().left;
+        let mouseY = event.pageY - $(event.target).offset().top;
+
+        for (let room of mapLocations) {
+            if (room.x1 <= mouseX && mouseX <= room.x2 && room.y1 <= mouseY && mouseY <= room.y2) {
+                changeToRoom(room.room);
+                break;
+            } 
+        }
+    });
 });
 
 function showInstructions() {
@@ -7,5 +45,13 @@ function showInstructions() {
 }
 
 function startGame() {
-    alert("Game started!");
+    changeToRoom("livingRoom");
+}
+
+function changeToRoom(room) {
+    if (room === currentRoom) return;
+
+    $(`#${currentRoom}`).fadeOut(fadeTime);
+    $(`#${room}`).fadeIn(fadeTime);
+    currentRoom = room;
 }
