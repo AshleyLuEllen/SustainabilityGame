7fff2739674c4f94a9d7a8994c9a71d88cfa8d70
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-02-22 12:22:54 -0600

    Main page and design

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $("#content").fadeIn(500);
+});
+
+function showInstructions() {
+    alert("Help message.");
+}
+
+function startGame() {
+    alert("Game started!");
+}
